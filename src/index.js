ab15d3b9ea3e2523f1e697d25aeb1e322dc21851
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-01-19 00:16:21 -0500

    style has to be on top and insert custom style on h1 style attribute

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const headStyle = {
+  color: "blue",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
 ReactDOM.render(
-  <h1 style={{ color: "red" }}>Hello World!</h1>,
+  <h1 style={headStyle}>Hello World!</h1>,
   document.getElementById("root")
 );
